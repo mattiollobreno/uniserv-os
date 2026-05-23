eb7a28817686264b15ff24b7e7904ce2661ccaa3
Jonas de Moraes <jonas.de.moraes0505@gmail.com>
2026-05-23 14:52:26 -0300

    AsyncHandler feito

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const asyncHandler = require('../middlewares/asyncHandler');
 const AuthRouthes = require('../controllers/authController');
 
 router.post('/cadastrar', asyncHandler(AuthRouthes.cadastrarUsuario));
